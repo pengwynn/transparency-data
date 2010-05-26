21dfd9a668c243f1aa47e4a29d2a496ba02d231d
Luigi Montanez <luigi.montanez@gmail.com>
2010-05-26 17:42:09 -0400

    Support calls for entities and politicians

--- lib/transparency_data/client.rb
+++ lib/transparency_data/client.rb
@@ -21,6 +21,65 @@ module TransparencyData
       end
     end
     
+    get(:entities) do |api_params|
+      uri TransparencyData.api_url("/entities")
+      params TransparencyData::Client.prepare_params(api_params)
+      handler do |response|
+        TransparencyData::Client.handle_response(response)
+      end
+    end
+    
+    get(:id_lookup) do |api_params|
+      uri TransparencyData.api_url("/entities/id_lookup")
+      params TransparencyData::Client.prepare_params(api_params)
+      handler do |response|
+        TransparencyData::Client.handle_response(response)
+      end
+    end
+    
+    get(:entity) do |id, api_params|
+      uri TransparencyData.api_url("/entities/#{id}")
+      params TransparencyData::Client.prepare_params(api_params) if api_params
+      handler do |response|
+        Hashie::Mash.new(JSON.parse(response.body))
+      end
+    end
+    
+    get(:top_contributors) do |id, api_params|
+      uri TransparencyData.api_url("/aggregates/pol/#{id}/contributors")
+      params TransparencyData::Client.prepare_params(api_params) if api_params
+      handler do |response|
+        TransparencyData::Client.handle_response(response)
+      end
+    end
+
+    get(:top_sectors) do |id, api_params|
+      uri TransparencyData.api_url("/aggregates/pol/#{id}/contributors/sectors")
+      params TransparencyData::Client.prepare_params(api_params) if api_params
+      handler do |response|
+        sectors = TransparencyData::Client.handle_response(response)
+        TransparencyData::Client.process_sectors(sectors)
+      end
+    end
+
+    get(:local_breakdown) do |id, api_params|
+      uri TransparencyData.api_url("/aggregates/pol/#{id}/contributors/local_breakdown")
+      params TransparencyData::Client.prepare_params(api_params) if api_params
+      handler do |response|
+        breakdown = Hashie::Mash.new(JSON.parse(response.body))
+        TransparencyData::Client.process_local_breakdown(breakdown)
+      end
+    end
+
+    get(:contributor_type_breakdown) do |id, api_params|
+      uri TransparencyData.api_url("/aggregates/pol/#{id}/contributors/type_breakdown")
+      params TransparencyData::Client.prepare_params(api_params) if api_params
+      handler do |response|
+        breakdown = Hashie::Mash.new(JSON.parse(response.body))
+        TransparencyData::Client.process_contributor_type_breakdown(breakdown)
+      end
+    end
+
     def self.prepare_params(params)
       params.each do |key, value|
         if value.is_a?(Hash)
@@ -45,6 +104,44 @@ module TransparencyData
       # TODO: raise_errors
       JSON.parse(response.body).map {|c| Hashie::Mash.new(c)}
     end
+    
+    def self.process_sectors(sectors)
+      sectors.each do |sector|
+        sector["name"] = case sector.sector
+          when "A" then "Agribusiness"
+          when "B" then "Communications/Electronics"
+          when "C" then "Construction"
+          when "D" then "Defense"
+          when "E" then "Energy/Natural Resources"
+          when "F" then "Finance/Insurance/Real Estate"
+          when "H" then "Health"
+          when "K" then "Lawyers/Lobbyists"
+          when "M" then "Transportation"
+          when "N" then "Misc. Business"
+          when "Q" then "Ideology/Single Issue"
+          when "P" then "Labor"
+          when "W" then "Other"
+          when "Y" then "Unknown"
+          when "Z" then "Administrative Use"
+        end
+      end
+    end
+    
+    def self.process_local_breakdown(breakdown)
+      breakdown.in_state_count      = breakdown["in-state"][0].to_i
+      breakdown.in_state_amount     = breakdown["in-state"][1].to_f
+      breakdown.out_of_state_count  = breakdown["out-of-state"][0].to_i
+      breakdown.out_of_state_amount = breakdown["out-of-state"][1].to_f
+      breakdown
+    end
+    
+    def self.process_contributor_type_breakdown(breakdown)
+      breakdown.individual_count  = breakdown["Individuals"][0].to_i
+      breakdown.individual_amount = breakdown["Individuals"][1].to_f
+      breakdown.pac_count  = breakdown["PACs"][0].to_i
+      breakdown.pac_amount = breakdown["PACs"][1].to_f
+      breakdown
+    end    
     
   end
 end
--- test/client_test.rb
+++ test/client_test.rb
@@ -24,30 +24,108 @@ class ClientTest < Test::Unit::TestCase
     context "when searching contributions" do
 
       should "return a list" do
-        VCR.use_cassette('contributions', :record => :all) do
+        VCR.use_cassette('contributions') do
           contributions = TransparencyData::Client.contributions(:contributor_ft => 'steve jobs')
-          contributions.class.to_s.should == 'Array'
+          contributions.class.should == Array
           assert contributions.first.has_key?("amount")
         end
       end
       
       should "find all the money Steve Jobs gave between March 3rd and March 10th 2009" do
-        VCR.use_cassette('contributions', :record => :all) do
+        VCR.use_cassette('contributions in date range') do
           contributions = TransparencyData::Client.contributions(:contributor_ft => 'steve jobs', :date => {:between => ["2009-03-04", "2009-03-10"]})
-          contributions.class.to_s.should == 'Array'
+          contributions.class.should == Array
         end
       end
+      
     end
     
     context "when searching lobbyists" do
 
       should "return a list of lobbying events" do
-        VCR.use_cassette('lobbying events', :record => :all) do
+        VCR.use_cassette('lobbying events') do
           lobbying = TransparencyData::Client.lobbying(:client_ft => "apple inc")
-          lobbying.class.to_s.should == 'Array'
+          lobbying.class.should == Array
         end
       end
+      
     end
+    
+    context "when searching entities" do
+      
+      should "return a list of entities" do
+        VCR.use_cassette('entities') do
+          entities = TransparencyData::Client.entities(:search => "nancy pelosi")
+          entities.class.should == Array
+        end
+      end
+      
+    end
+
+    context "when looking up by entity ids" do
+      
+      should "return a list of entity ids" do
+        VCR.use_cassette('entity ids') do
+          entity_ids = TransparencyData::Client.id_lookup(:namespace => "urn:crp:recipient", :id => "N00007360")
+          entity_ids.class.should == Array
+        end
+      end
+      
+    end
+
+    context "when getting one entity" do
+      
+      should "return an entity" do
+        VCR.use_cassette('entity') do
+          entity = TransparencyData::Client.entity("ff96aa62d48f48e5a1e284efe74a0ba8")
+          entity.name.should == "Nancy Pelosi (D)"
+        end
+      end
+      
+    end
+    
+    context "politician methods" do
+      
+      should "return a list of top contributors" do
+        VCR.use_cassette('top contributors') do
+          contributors = TransparencyData::Client.top_contributors("ff96aa62d48f48e5a1e284efe74a0ba8")
+          contributors.class.should == Array
+          contributors.length.should == 10
+        end
+      end
+      
+      should "return a list of 5 top contributors" do
+        VCR.use_cassette('top 5 contributors') do
+          contributors = TransparencyData::Client.top_contributors("ff96aa62d48f48e5a1e284efe74a0ba8", :limit => 5)
+          contributors.class.should == Array
+          contributors.length.should == 5
+        end
+      end
+      
+      should "return a list of top sectors" do
+        VCR.use_cassette('top 5 contributors') do
+          sectors = TransparencyData::Client.top_sectors("ff96aa62d48f48e5a1e284efe74a0ba8")
+          sectors.class.should == Array
+          sectors.first.name.should == "Finance/Insurance/Real Estate"
+        end
+      end
+
+      should "return a local breakdown" do
+        VCR.use_cassette('local breakdown') do
+          local_breakdown = TransparencyData::Client.local_breakdown("ff96aa62d48f48e5a1e284efe74a0ba8")
+          local_breakdown.in_state_amount.class.should == Float
+        end
+      end
+
+      should "return a contributor type breakdown" do
+        VCR.use_cassette('contributor type breakdown') do
+          local_breakdown = TransparencyData::Client.contributor_type_breakdown("ff96aa62d48f48e5a1e284efe74a0ba8")
+          local_breakdown.individual_count.class.should == Fixnum
+        end
+      end
+
+    end
+
   end
   
 end
